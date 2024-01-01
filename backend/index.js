5602b5b5c89532d960f2b9c956f340608bf4f51b
RameshBabuRb <150510357+RameshBabuRb@users.noreply.github.com>
2024-01-01 22:49:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,11 @@ const corsOptions = {
     optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 
-app.use(cors(corsOptions));
+app.use(cors({
+  orgin:["https://ramesh-projects-frontend.vercel.app"],
+  method:["POST","GET"],
+  credential:true
+}));
 
 
 app.get('/', (req, res) => {
